683d5e98bcd3bd8f4a5037c3ede0996b14a14fbb
reikavik <118544840+reikavik@users.noreply.github.com>
2023-07-20 20:17:31 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function MobileMenu() {
+    document.getElementById("mobile-menu").style.display = "block";
+    document.getElementById("mobile-menu-button").style.display = "none";
+    document.getElementById("mobile-menu-close-button").style.display = "inline";
+}
+
+function MobileMenuClose() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.getElementById("mobile-menu-button").style.display = "inline";
+    document.getElementById("mobile-menu-close-button").style.display = "none";
+
+}
