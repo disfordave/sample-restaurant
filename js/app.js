6a383410d837d89e1d45e500485542930fad68ed
reikavik <118544840+reikavik@users.noreply.github.com>
2023-07-21 23:19:38 +0900

    Add fixed header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var vw  = document.documentElement.clientWidth;
+let vw  = document.documentElement.clientWidth;
+
 
 console.log(vw)
 
@@ -23,14 +24,18 @@ function MobileMenu() {
     document.getElementById("mobile-menu-close-button").style.display = "inline";
     document.getElementById("logo").style.color = "black";
     document.body.style.overflow = "hidden";
-    window.scrollTo(0, 0); // Scroll to the top of the page
+    // window.scrollTo(0, 0); // Scroll to the top of the page
 }
 
 function MobileMenuClose() {
     document.getElementById("mobile-menu").style.display = "none";
     document.getElementById("mobile-menu-button").style.display = "inline";
     document.getElementById("mobile-menu-close-button").style.display = "none";
-    document.getElementById("logo").style.color = "aliceblue";
+    if (document.documentElement.scrollTop > 50) {
+        document.getElementById("logo").style.color = "black";
+    } else {
+        document.getElementById("logo").style.color = "aliceblue";
+    }
     document.body.style.overflow = "auto";
 }
 
@@ -39,3 +44,32 @@ window.addEventListener('resize', checkWindowSize);
 
 // Check window size initially when the script loads
 checkWindowSize();
+
+/*window.scrollY > 50*/
+window.onscroll = function() {onWindowScroll()};
+
+function onWindowScroll() {
+    if (document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.backgroundColor = "aliceblue";
+        document.getElementById("logo").style.color = "black";
+        document.getElementsByClassName("nav-a")[0].style.color = "black"
+        document.getElementsByClassName("nav-a")[1].style.color = "black"
+        document.getElementsByClassName("nav-a")[2].style.color = "black"
+        document.getElementsByClassName("nav-a")[3].style.color = "black"
+        
+    } else {
+        document.getElementById("header").style.backgroundColor = "transparent";
+        document.getElementById("logo").style.color = "aliceblue";
+        document.getElementsByClassName("nav-a")[0].style.color = "aliceblue"
+        document.getElementsByClassName("nav-a")[1].style.color = "aliceblue"
+        document.getElementsByClassName("nav-a")[2].style.color = "aliceblue"
+        document.getElementsByClassName("nav-a")[3].style.color = "aliceblue"
+        
+    }
+}
+
+// window.addEventListener('scroll', onWindowScroll);
+
+
+
+
